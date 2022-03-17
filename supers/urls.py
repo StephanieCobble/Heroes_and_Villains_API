bd748846b4ca6d7e2daa70551a44f3c22f7bb2ca
StephanieCobble <cobble.stephanie@gmail.com>
2022-03-17 13:05:35 -0700

    views and urls are more set up for supers. Updated main settings and urls.

--- supers/urls.py
+++ supers/urls.py
@@ -1,11 +1,11 @@
-# from django.urls import path
-# from rest_framework.urlpatterns import format_suffix_patterns 
-# from .import views
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns 
+from .import views
 
-# urlpatterns = [
-#     path('', views.SuperList.as_view()),
-#     path('<int:pk>/', views.SuperDetail.as_view()),
-#     path('fk/<int:fk>/', views.SuperFK.as_view())
-# ]
+urlpatterns = [
+    path('', views.SuperList.as_view()),
+    path('<int:pk>/', views.SuperDetail.as_view()),
+    path('fk/<int:fk>/', views.SuperFK.as_view())
+]
 
-# urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns(urlpatterns)
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
 
 # Create your views here.
+from django.http import Http404 
+from rest_framework.views import APIView  
+
+from rest_framework.response import Response  
+from .serializers import SuperSerializer  
+from .models import Super  
+from rest_framework import status  
+
+
+class SuperList(APIView):
+    def get(self, request, format=None):
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class SuperDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Super.objects.get(pk=pk)  
+        except Super.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        super = self.get_object(pk)
+        serializer = SuperSerializer(super)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        super = self.get_object(pk)
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request, pk, format=None):
+        super = self.get_object(pk)
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class SuperFK(APIView):
+    def get(self, request, fk, format=None):
+        super = Super.objects.filter(super_type=fk)
+        serializer = SuperSerializer(super, many=True)
+        return Response(serializer.data)
