d41aa813442ee46bd73d4967c7eae636a32220f1
StephanieCobble <cobble.stephanie@gmail.com>
2022-03-21 14:59:31 -0700

    added model and endpoint to compare heroes and villains that should result in win, lose, or draw. Updated urls. Still in progress. Need to work on serializer refs

--- supers/urls.py
+++ supers/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.SuperList.as_view()),
     path('<int:pk>/', views.SuperDetail.as_view()),
     path('<int:pk>/<int:pk2>/', views.SuperDetail.as_view()),
+    path('<str:super_one>/<str:super_two>/', views.SuperFight.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- supers/views.py
+++ supers/views.py
@@ -12,6 +12,7 @@ from super_types.models import SuperType
 from .serializers import SuperSerializer  
 from .models import Power, Super  
 from rest_framework import status  
+from django.db.models import Count
 
 
 class SuperList(APIView):
@@ -81,14 +82,56 @@ class SuperDetail(APIView):
         super.powers.add(power)
         serializer = SuperSerializer(super)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+# Bonus 2:
+# •	Create an endpoint that allows you to pass in a hero name and villain name as Query params.
+# •	Query for each of the submitted Supers and compare their number of powers. Whoever has more powers listed is the winner
+# •	Send back a custom object response that contains a 'winner' key containing the winner's info and a 'loser' key containing the loser's info, or a different message if it is a tie.
+
+
+class SuperFight(APIView):
+
+    def get_super(self, super):
+        try:
+            return Super.objects.get(name=super)  
+        except Super.DoesNotExist:
+            raise Http404
     
+    def hero_vs_villain(self, super_one, super_two):
+        super_type_one = super_one.super_type
+        super_type_two = super_two.super_type
+        if super_type_one == super_type_two:
+            False
+        else:
+            return True
 
+    def power_count(self, super):
+        power_count_result = Super.objects.annotate(power_number=Count('powers')).get(id=super.id)
+        count = power_count_result.power_number
+        return count
 
-
-# class SuperFK(APIView):
-#     def get(self, request, fk, format=None):
-#         super = Super.objects.filter(super_type=fk)
-#         serializer = SuperSerializer(super, many=True)
-#         return Response(serializer.data)
-
-
+    def get(self, request, super_one, super_two):
+        super_one = self.get_super(super_one)
+        super_two = self.get_super(super_two)
+        battle = self.hero_vs_villain(super_one, super_two)
+        if battle == False:
+            if super_one.super_type_id == 1:
+                return Response(f'You must battle against an opponent!')
+            else:
+                return Response(f'You must battle again an opponent!')
+        super_one_count = self.power_count(super_one)
+        super_two_count = self.power_count(super_two)
+        if super_one_count == super_two_count:
+            custom_battle_response = "Draw!"
+        elif super_one_count > super_two_count:
+            win_serializer = SuperSerializer(super_one)
+            lose_serializer = SuperSerializer(super_two)
+        else:
+            win_serializer = SuperSerializer(super_two)
+            lose_serializer = SuperSerializer(super_one)
+        custom_battle_response = {
+            "Winner!": win_serializer.data,
+            "Loser!": lose_serializer.data
+        }
+        return Response(custom_battle_response, status=status.HTTP_200_OK)
